88cf1174fba0b79b6d3923c71cd3d513f5f920ce
hsiaor <hsiaor@oregonstate.edu>
2024-05-18 20:46:37 -0500

    rename files

--- celcius-service.py
+++ celcius-service.py
@@ -4,12 +4,12 @@ import os
 file_path = os.path.join(os.path.dirname(__file__), 'temperature.txt')
 
 # Read the Fahrenheit value from the file
-with open('farenheit-to-celcius.txt', 'r') as file:
+with open('fahrenheit-to-celcius.txt', 'r') as file:
     fahrenheit = float(file.read().strip())
 
 # Convert Fahrenheit to Celsius
 celsius = round((fahrenheit - 32) * 5/9)
 
 # Write the Celsius value back to the file
-with open('farenheit-to-celcius.txt', 'w') as file:
+with open('fahrenheit-to-celcius.txt', 'w') as file:
     file.write(str(celsius))
